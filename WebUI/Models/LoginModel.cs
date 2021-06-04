66bb472482513141a3e83a09d5f311b2d8ab089c
Adithya Vijay K <adithyavj@gmail.com>
2021-06-05 00:33:14 +0530

    Add login page Model,View and Controller

---
+++ WebUI/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebUI.Models
+{
+    public class LoginModel
+    {
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public int RememberMe { get; set; }
+    }
+}
