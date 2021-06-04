5729d3594e7fe64b9a8aec5f456a3c9c57235c3f
Adithya Vijay K <adithyavj@gmail.com>
2021-06-05 01:24:10 +0530

    Removed WebUI and Added new WebBasedUI with Individual Authentication. Added UserName in Regstration and Login

---
+++ WebBasedUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebBasedUI.Startup))]
+namespace WebBasedUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
