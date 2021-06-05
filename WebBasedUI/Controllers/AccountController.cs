3fa62da98def6245ce502ee2fd3b098dadf39120
Adithya Vijay K <adithyavj@gmail.com>
2021-06-05 19:45:22 +0530

    Add FirstName,LastName columns in DataBase, Model,View. Rename EntityFramework Db and its connectionstring name.

--- WebBasedUI/Controllers/AccountController.cs
+++ WebBasedUI/Controllers/AccountController.cs
@@ -151,7 +151,14 @@ namespace WebBasedUI.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.UserName, Email = model.Email };
+                // add firstname and lastname
+                var user = new ApplicationUser
+                {
+                    UserName = model.UserName,
+                    Email = model.Email,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName
+                };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
--- WebBasedUI/Models/AccountViewModels.cs
+++ WebBasedUI/Models/AccountViewModels.cs
@@ -76,6 +76,13 @@ namespace WebBasedUI.Models
         [Display(Name = "User Name")]
         public string UserName { get; set; }
 
+        [Display(Name = "First Name"), Required]
+        [StringLength(100)]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
