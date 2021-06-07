ddc72734a8378bf39186d2a46a93e15a2ec53ad5
Adithya Vijay K <adithyavj@gmail.com>
2021-06-07 11:49:41 +0530

    Added DB to store userdata, add connectionstring in web.config, DataAccess methods added in ClassLibrary

---
+++ Data/dbo/Tables/InterviewCall.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[InterviewCall]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [CallDate] DATETIME2 NOT NULL, 
+    [ContactedPerson] NVARCHAR(50) NULL, 
+    [PhoneNumber] NVARCHAR(12) NULL, 
+    [Company] NVARCHAR(50) NOT NULL, 
+    [Location] NVARCHAR(50) NOT NULL, 
+    [IsShortListed] BIT NOT NULL DEFAULT 0, 
+    [InterviewDate] DATETIME2 NULL, 
+    [Remarks] NVARCHAR(MAX) NULL
+)
---
+++ DataLibrary/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace DataLibrary.DataAccess
+{
+    public class SqlDataAccess
+    {
+        // Connection String
+        public static string GetConnectionString(string connectionName= "ScheduleData")
+        {
+            return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+        }
+
+        // Save, Edit, Delete
+        public static int ManipulateData<T>(string storedProcedure,T paramters)
+        {
+            using (IDbConnection connection = new SqlConnection(GetConnectionString()))
+            {
+                return connection.Execute(storedProcedure, paramters, 
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        // Select / Read
+        public static List<T> LoadData<T,U>(string storedProcedure, U paramters)
+        {
+            using (IDbConnection connection = new SqlConnection(GetConnectionString()))
+            {
+                // This returns Ienumerable, but we convert it to List and Return List<T> - List of Model
+                return connection.Query<T>(storedProcedure, paramters, 
+                    commandType: CommandType.StoredProcedure).ToList();
+            }
+        }
+
+    }
+}
